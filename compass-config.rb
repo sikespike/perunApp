2eca27ce3550bc90ba37bceb20a04e38dcf89a37
sikespike <vkavoun@gmail.com>
2015-06-25 02:44:46 -0400

    gradual configuration of the project.
    reconfigured it to use sass instead of less

---
+++ compass-config.rb
@@ -0,0 +1,11 @@
+Encoding.default_external = "utf-8"
+
+css_dir = "./build/styles"
+images_dir = "./resources/images"
+javascripts_dir = "lib"
+sass_dir = "./app/styles"
+relative_assets = 'true'
+
+environment = :development
+
+disable_warnings = false
--- tasks/build.js
+++ tasks/build.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var less = require('gulp-less');
-var esperanto = require('esperanto');
-var map = require('vinyl-map');
-var jetpack = require('fs-jetpack');
+var gulp = require("gulp");
+var compass = require("gulp-sass");
+var esperanto = require("esperanto");
+var map = require("vinyl-map");
+var jetpack = require("fs-jetpack");
 
-var utils = require('./utils');
+var utils = require("./utils");
 
 var projectDir = jetpack;
-var srcDir = projectDir.cwd('./app');
-var destDir = projectDir.cwd('./build');
+var srcDir = projectDir.cwd("./app");
+var destDir = projectDir.cwd("./build");
 
 var paths = {
     jsCodeToTranspile: [
-        'app/**/*.js',
-        '!app/node_modules/**',
-        '!app/bower_components/**',
-        '!app/vendor/**'
+        "app/**/*.js",
+        "!app/node_modules/**",
+        "!app/bower_components/**",
+        "!app/vendor/**"
     ],
     toCopy: [
-        'app/node_modules/**',
-        'app/bower_components/**',
-        'app/vendor/**',
-        'app/**/*.html'
+        "app/node_modules/**",
+        "app/bower_components/**",
+        "app/vendor/**",
+        "app/**/*.html"
     ],
 }
 
@@ -31,21 +31,21 @@ var paths = {
 // Tasks
 // -------------------------------------
 
-gulp.task('clean', function(callback) {
-    return destDir.dirAsync('.', { empty: true });
+gulp.task("clean", function(callback) {
+    return destDir.dirAsync(".", { empty: true });
 });
 
 
 var copyTask = function () {
-    projectDir.copy('resources/icon.png', destDir.path('icon.png'), { overwrite: true });
+    projectDir.copy("resources/icon.png", destDir.path("icon.png"), { overwrite: true });
 
-    return projectDir.copyAsync('app', destDir.path(), {
+    return projectDir.copyAsync("app", destDir.path(), {
         overwrite: true,
         matching: paths.toCopy
     });
 };
-gulp.task('copy', ['clean'], copyTask);
-gulp.task('copy-watch', copyTask);
+gulp.task("copy", ["clean"], copyTask);
+gulp.task("copy-watch", copyTask);
 
 
 var transpileTask = function () {
@@ -54,58 +54,62 @@ var transpileTask = function () {
         try {
             var transpiled = esperanto.toAmd(code.toString(), { strict: true });
         } catch (err) {
-            throw new Error(err.message + ' ' + filename);
+            throw new Error(err.message + " " + filename);
         }
         return transpiled.code;
     }))
     .pipe(gulp.dest(destDir.path()));
 };
-gulp.task('transpile', ['clean'], transpileTask);
-gulp.task('transpile-watch', transpileTask);
+gulp.task("transpile", ["clean"], transpileTask);
+gulp.task("transpile-watch", transpileTask);
 
 
-var lessTask = function () {
-    return gulp.src('app/stylesheets/main.less')
-    .pipe(less())
-    .pipe(gulp.dest(destDir.path('stylesheets')));
+var sassTask =function() {
+    return gulp.src("./app/styles/*.scss")
+        .pipe(compass({
+            config_file: "./compass-config.rb",
+            css: "./build/styles",
+            sass: "./app/styles"
+        })).pipe(gulp.dest(destDir.path("styles")));
 };
-gulp.task('less', ['clean'], lessTask);
-gulp.task('less-watch', lessTask);
+
+gulp.task("compass", ["clean"], sassTask);
+gulp.task("compass-watch", sassTask);
 
 
 // Add and customize OS-specyfic and target-specyfic stuff.
-gulp.task('finalize', ['clean'], function () {
-    var manifest = srcDir.read('package.json', 'json');
+gulp.task("finalize", ["clean"], function () {
+    var manifest = srcDir.read("package.json", "json");
     switch (utils.getEnvName()) {
-        case 'production':
+        case "production":
             // Hide dev toolbar if doing a release.
             manifest.window.toolbar = false;
             break;
-        case 'test':
+        case "test":
             // Add "-test" suffix to name, so NW.js will write all
             // data like cookies and locaStorage into separate place.
-            manifest.name += '-test';
+            manifest.name += "-test";
             // Change the main entry to spec runner.
-            manifest.main = 'spec.html';
+            manifest.main = "spec.html";
             break;
-        case 'development':
+        case "development":
             // Add "-dev" suffix to name, so NW.js will write all
             // data like cookies and locaStorage into separate place.
-            manifest.name += '-dev';
+            manifest.name += "-dev";
             break;
     }
-    destDir.write('package.json', manifest);
+    destDir.write("package.json", manifest);
 
-    var configFilePath = projectDir.path('config/env_' + utils.getEnvName() + '.json');
-    destDir.copy(configFilePath, 'env_config.json');
+    var configFilePath = projectDir.path("config/env_" + utils.getEnvName() + ".json");
+    destDir.copy(configFilePath, "env_config.json");
 });
 
 
-gulp.task('watch', function () {
-    gulp.watch(paths.jsCodeToTranspile, ['transpile-watch']);
-    gulp.watch(paths.toCopy, ['copy-watch']);
-    gulp.watch('app/**/*.less', ['less-watch']);
+gulp.task("watch", function () {
+    gulp.watch(paths.jsCodeToTranspile, ["transpile-watch"]);
+    gulp.watch(paths.toCopy, ["copy-watch"]);
+    gulp.watch("app/**/*.scss", ["compass-watch"]);
 });
 
 
-gulp.task('build', ['transpile', 'less', 'copy', 'finalize']);
+gulp.task("build", ["transpile", "compass", "copy", "finalize"]);
