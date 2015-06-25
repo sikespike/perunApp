214a30fbd63d76c8f3ab8849da13013d5d2ea380
sikespike <vkavoun@gmail.com>
2015-06-25 04:52:11 -0400

    incorporating requirejs into the project and jquery

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
-// -----------------------------------------------------
-// Here is the starting point for your own code.
-// All stuff below is just to show you how it works.
-// -----------------------------------------------------
-
-// Browser modules are imported through new ES6 syntax.
-import { greet } from './hello_world/hello_world';
-
-// Node modules are required the same way as always.
 var os = require('os');
+var requirejs = require('requirejs');
 
-// window.env contains data from config/env_XXX.json file.
-var envName = window.env.name;
+function startApp() {
+    requirejs.config({
+        baseUrl: "./lib",
+        paths: {
+            "model": "model/",
+            "net": "net/",
+            "ui": "ui/"
+        }
+    });
 
+    requirejs(['app'], function (app) {
+
+    });
+}
