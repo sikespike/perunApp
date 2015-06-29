6d293f590e4ead2e12f831dcee3d44a8e7330e54
sikespike <vkavoun@gmail.com>
2015-06-29 19:55:25 -0400

    moving nwjs gui window code

--- app/app.js
+++ app/app.js
@@ -8,6 +8,16 @@ function startApp() {
 }
 
 $(function(){
+    var gui = require("nw.gui");
+    var win = gui.Window.get();
+
+    // Create default menu items for OSX
+    if (process.platform === "darwin") {
+        var mb = new gui.Menu({ type: "menubar" });
+        mb.createMacBuiltin(gui.App.manifest.productName);
+        win.menu = mb;
+    }
+
     $(document).ready(function(){
         startApp.call(this);
     });
