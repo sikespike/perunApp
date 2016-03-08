dcc56242a3c5eb2e3e0ee2ffde48b2ef30661a1e
sikespike <vkavoun@gmail.com>
2016-03-08 18:13:26 -0500

    fixing errors

--- app/lib/api/actionsApi.js
+++ app/lib/api/actionsApi.js
@@ -2,7 +2,7 @@ var $ = require("jquery");
 var _ = require("underscore");
 
 import {Event} from "../event/event.js";
-import {dispatchManager} from "./dispatchManager.js";
+import {dispatchManager} from "../managers/dispatchManager.js";
 
 function ActionsApiService() {
     this.init();
